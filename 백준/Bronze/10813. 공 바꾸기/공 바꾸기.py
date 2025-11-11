34f659614cdad1fc97dc2413ed789f2f5cf5f3f4
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-11 22:24:23 +0900

    [Bronze II] Title: 공 바꾸기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+
+basket = list(range(1, n + 1))
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
+    
+print(" ".join(map(str, basket)))
